91de50d149d2e47f14dd60d23fb693e2375d120e
Mihsael <136724162+Mihsael@users.noreply.github.com>
2023-06-15 17:27:54 +0300

    Add files via upload

---
+++ HelloWorld.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HelloWorld : MonoBehaviour
+{
+    // При запуске игры
+    void Start()
+    {
+        // Привет мир
+        print("HelloWorld");
+    }
+
+    // Всё время
+    void Update()
+    {
+        
+    }
+}
